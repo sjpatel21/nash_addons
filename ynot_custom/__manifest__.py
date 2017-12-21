5767801f29225f5f934eaaee769d0f44a59e3bfc
sjpatel21 <sunny.odoo@gmail.com>
2017-12-21 08:31:14 +0530

    ADD FACTORY APP

--- ynot_custom/__manifest__.py
+++ ynot_custom/__manifest__.py
@@ -24,6 +24,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
+        'views/factory_view.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- ynot_custom/models/models.py
+++ ynot_custom/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class ynot_project(models.Model):
     _name = 'ynot.project'
 
@@ -8,22 +9,27 @@ class ynot_project(models.Model):
     def _compute_currency(self):
         self.currency_id = self.company_id.currency_id
 
-
     name = fields.Char("Project Title", required=True)
     create_date = fields.Datetime('Created', readonly=True)
-    create_uid = fields.Many2one('res.users', string='Completed By', readonly=True) 
+    create_uid = fields.Many2one(
+        'res.users', string='Completed By', readonly=True)
     assigned_to = fields.Many2one('res.users', string='Assigned To',)
     assigned_date = fields.Date('Date')
     client_company_id = fields.Many2one('res.partner', string='Company', domain="[('customer','=',True),('is_company','=',True)]",
-                                context="{'search_default_customer':1, 'show_address': 1}")
-    client_id = fields.Many2one('res.partner', string='Client', domain="[('customer','=',True),('is_company','=',False)]",)
-    revenue_opportunity = fields.Monetary(currency_field='currency_id', string="Revenue Opportunity")
-    currency_id = fields.Many2one('res.currency', compute='_compute_currency', string="Currency")
+                                        context="{'search_default_customer':1, 'show_address': 1}")
+    client_id = fields.Many2one('res.partner', string='Client',
+                                domain="[('customer','=',True),('is_company','=',False)]",)
+    revenue_opportunity = fields.Monetary(
+        currency_field='currency_id', string="Revenue Opportunity")
+    currency_id = fields.Many2one(
+        'res.currency', compute='_compute_currency', string="Currency")
     company_id = fields.Many2one('res.company', string='Company', store=True, readonly=True,
-        default=lambda self: self.env['res.company']._company_default_get(''))
+                                 default=lambda self: self.env['res.company']._company_default_get(''))
 
-    price_range_from = fields.Monetary(currency_field='currency_id', string="Price Range From")
-    price_range_to = fields.Monetary(currency_field='currency_id', string="Price Range To")
+    price_range_from = fields.Monetary(
+        currency_field='currency_id', string="Price Range From")
+    price_range_to = fields.Monetary(
+        currency_field='currency_id', string="Price Range To")
 
     project_overview = fields.Text("Project Overview")
 
@@ -42,12 +48,14 @@ class ynot_project(models.Model):
     task_cust_po = fields.Boolean(string='Customer PO or Contract')
     task_charges = fields.Boolean(string='Charges to Customer')
 
-
-    task_concept_add = fields.Selection([('duplicate','Duplicate'),('graphic','Graphic'),('pacakaging','Pacakaging'),('product','Product'),('other','Other')], string="Concept Additional Info")
-    task_quote_overseas_add = fields.Selection([('india','India'),('china','China'),('other','Other'),], string="Overseas Additional Info")
+    task_concept_add = fields.Selection([('duplicate', 'Duplicate'), ('graphic', 'Graphic'), (
+        'pacakaging', 'Pacakaging'), ('product', 'Product'), ('other', 'Other')], string="Concept Additional Info")
+    task_quote_overseas_add = fields.Selection(
+        [('india', 'India'), ('china', 'China'), ('other', 'Other'), ], string="Overseas Additional Info")
     task_overseas_other = fields.Char("Other Country")
-    
-    target_audience = fields.Text("Target Audience", help="The who, what, when, and where of the target customer base")
+
+    target_audience = fields.Text(
+        "Target Audience", help="The who, what, when, and where of the target customer base")
     campaign_look = fields.Text("Campaign Look & Feel")
     competitive_analysis = fields.Text("Competitive Analysis")
 
@@ -65,7 +73,6 @@ class ynot_project(models.Model):
     pro_tl_text_3 = fields.Char("Projected Timeline Desc 3")
     pro_tl_text_4 = fields.Char("Projected Timeline Desc 4")
 
-
     imp_tl_date_1 = fields.Date("Date 1")
     imp_tl_date_2 = fields.Date("Date 2")
     imp_tl_date_3 = fields.Date("Date 3")
@@ -76,10 +83,9 @@ class ynot_project(models.Model):
     imp_tl_text_3 = fields.Char("Important Timeline Desc 3")
     imp_tl_text_4 = fields.Char("Important Timeline Desc 4")
 
-
     other_desc = fields.Text("Other Info")
     client_contact_id = fields.Many2one('res.partner', string='Client',
-                                context="{'show_phone': 1}")
+                                        context="{'show_phone': 1}")
 
     notes = fields.Text("Notes")
     images = fields.One2many('project.image', 'project_id', string='Images')
@@ -98,7 +104,7 @@ class project_image(models.Model):
     sequence = fields.Integer('Sequence')
     image = fields.Binary('Image')
     project_id = fields.Many2one('ynot.project', string='Project')
-    
+
 
 class res_partner(models.Model):
     _inherit = 'res.partner'
@@ -107,9 +113,9 @@ class res_partner(models.Model):
     def name_get(self):
         # res = super(res_partner,self).name_get()
         res = super(res_partner, self).name_get()
-        print("OR name_get------------res-----------",res)
+        print("OR name_get------------res-----------", res)
         final_res = []
-        
+
         for rec in res:
             name = rec[1]
             partner = self.browse(rec[0])
@@ -117,5 +123,147 @@ class res_partner(models.Model):
             if self._context.get('show_phone') and partner.phone:
                 name = "%s <%s>" % (name, partner.phone)
             final_res.append((partner.id, name))
-        print( "final_res-----------",final_res)
+        print("final_res-----------", final_res)
         return final_res
+
+
+class YnotFactory(models.Model):
+    _name = 'ynot.factory'
+
+    name = fields.Char(
+        string='Company Name',
+        required=True,
+    )
+    company_address = fields.Text(
+        string='Company Address',
+    )
+    nearest_port = fields.Char("Nearest Port")
+    major_airport = fields.Char("Nearest Major Airport")
+    year_est = fields.Integer("Year Est")
+    no_of_emp = fields.Integer("No. of Employees")
+    loc_fun = fields.Text('Location or Functions')
+    company_website = fields.Char("Company Website")
+    company_cert = fields.Char("Company Certifications")
+    ecommerce = fields.Char("Ecommerce")
+    is_catelogs = fields.Boolean("Catalogs")
+    catalog_updated = fields.Char("How often updated ?")
+
+    # General Financial Info
+    sale_ten_three = fields.Float("Sale from 10 Yrs Ago to Last 3 Yrs")
+    sale_three_three = fields.Float("Sale from 3 Yrs Ago to Last 3 Yrs")
+    sale_two_three = fields.Float("Sale from 2 Yrs Ago to Last 3 Yrs")
+    sale_last_year = fields.Float("Sale from Last Year")
+
+    # Government Licenses
+    local_lic = fields.Char("Local")
+    state_lic = fields.Char("State")
+    federal_lic = fields.Char("Federal")
+
+    # Shipping Info
+    methods_shipping = fields.Char("Methods of Shipping")
+    fob_ship = fields.Char("FOB")
+    exworks_ship = fields.Char("EX-WORKS")
+
+    # Clinet Info
+
+    major_client_ids = fields.One2many(
+        string='Major Clients',
+        help="What percentage of their business is with each company; and which countries are these clients from",
+        comodel_name='major.clients',
+        inverse_name='factory_id',
+    )
+
+    top_country_ids = fields.Many2many(
+        string='Top Country',
+        comodel_name='res.country',
+        relation='factory_top_country',
+        column1='factory_id',
+        column2='res_country_id',
+    )
+
+    # misc info
+    sample_charges = fields.Char("Sample Charges (or Free)")
+    con_orders = fields.Char("Consolidation of Orders")
+    con_cont = fields.Char("Consolidation of Containers")
+    prod_testing = fields.Char("Product Testing")
+    is_prd_engg = fields.Boolean("Engineering")
+    is_threed_modelling = fields.Boolean("3D Modelling")
+    acceptable_formats = fields.Char("List of Acceptable Formats")
+    is_accept_terms = fields.Boolean("Do you accept Y-Not's terms (listed)?")
+
+    # Contact Info
+    factory_contact_ids = fields.One2many(
+        string='Factory Contacts',
+        comodel_name='factory.contact',
+        inverse_name='factory_id',
+    )
+
+    # business info
+    material_ids = fields.Many2many(
+        string='Materials',
+        comodel_name='factory.material',
+        relation='factory_res_material',
+        column1='factory_id',
+        column2='material_id',
+    )
+    product_cat_ids = fields.Many2many(
+        string='Product Categories/ Ranges',
+        comodel_name='factory.prdcat',
+        relation='factory_res_prdcat',
+        column1='factory_id',
+        column2='prdcat_id',
+    )
+    samples = fields.Char("Samples")
+    production = fields.Char("Production")
+    sample_lead_time = fields.Char("Samples")
+    sample_prd_time = fields.Char("Production")
+
+    # internal use
+    ynot_contact_person = fields.Many2one("res.users", "Y-not Contact Person")
+    english = fields.Selection(
+        string='English',
+        selection=[('1', 'Very Low'), ('2', 'Low'),
+                   ('3', 'Normal'), ('4', 'High'), ('5', 'Very High')]
+    )
+    cleanliness = fields.Selection(
+        string='Cleanlinesss',
+        selection=[('1', 'Very Low'), ('2', 'Low'),
+                   ('3', 'Normal'), ('4', 'High'), ('5', 'Very High')]
+    )
+
+
+class YnotMaterial(models.Model):
+    _name = 'factory.material'
+
+    name = fields.Char("Material")
+
+
+class YnotProductCat(models.Model):
+    _name = 'factory.prdcat'
+
+    name = fields.Char("Product Category")
+
+
+class YnotMajorClient(models.Model):
+    _name = 'major.clients'
+
+    factory_id = fields.Many2one('ynot.factory', string='Factory',)
+    name = fields.Char("Name", required=True)
+    per_of_business = fields.Float("Percentage of Business")
+    country_id = fields.Many2one('res.country', 'Country')
+    sequence = fields.Integer('Sequence')
+
+
+class YnotFactoryContact(models.Model):
+    _name = 'factory.contact'
+
+    factory_id = fields.Many2one('ynot.factory', string='Factory',)
+    name = fields.Char("Contact Name", required=True)
+    email = fields.Float("Email")
+    name = fields.Char("City")
+    country_id = fields.Many2one('res.country', 'Country')
+    company_contact = fields.Char("Company Contact")
+    sale_managers = fields.Char("Sale Managers")
+    head_of_aperation = fields.Char("Head of Operations")
+    c_level_position = fields.Char("C-Level Positions")
+    owner = fields.Char("Owner(s)")
